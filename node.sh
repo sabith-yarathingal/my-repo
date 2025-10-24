675d93ffda9e9f6f6a2a34f9f3a3e671037050ba
sabith-yarathingal <sabithy786@gmail.com>
2025-10-24 20:15:12 +0530

    this is my first sample script

---
+++ node.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+###################
+#author : sabith
+#date : 23-10-2025
+#
+#this is a script output the node health
+#
+#version :
+#
+###################
+
+set -x
+
+df -h
+free -g
+nproc
+
+ 
